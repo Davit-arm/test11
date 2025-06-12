9c2729254fa1ac1ccf9d567e55a860786e125f3f
Davit <davidseraydaryan@gmail.com>
2025-06-12 18:25:17 +0200

    homework

--- bot.py
+++ bot.py
@@ -35,4 +35,9 @@ def url_ban(message):
 def info(message):
     bot.reply_to(message, "im a bot that can ban people")
 
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
 bot.infinity_polling(none_stop=True)
