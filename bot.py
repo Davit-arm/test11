1f29e7b3765591bb652108e5567dfa668dfd7f42
Davit <davidseraydaryan@gmail.com>
2025-06-12 18:20:56 +0200

    update 1

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Привет! Я бот для управления чатомa.")
+    bot.reply_to(message, "Привет! Я бот для управления чатом.")
 
 @bot.message_handler(commands=['ban'])
 def ban_user(message):
@@ -22,5 +22,17 @@ def ban_user(message):
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+
+@bot.message_handler(func=lambda message: message.text.startswith('https://'))
+def url_ban(message):
+    if message.reply_to_message:
+        chat_id = message.chat.id
+        user_id = message.reply_to_message.from_user.id
+        bot.ban_chat_member(chat_id, user_id)
+        bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
+    
+@bot.message_handler(commands=['info'])
+def info(message):
+    bot.reply_to(message, "im a bot that can ban people")
 
 bot.infinity_polling(none_stop=True)
